2e1683435f01dd041de7c828700abe84b6c49f76
David Mansy <david.mansy@icloud.com>
2020-08-18 23:43:18 +0200

    Implement design of the overal page, search input and autocomplete results

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { getPackages } from "./api";
 import useDebounce from "./hooks/useDebounce";
 import "./App.css";
+import { ReactComponent as Logo } from "./assets/logo.svg";
 
 // TODO: Unit test
 // TODO: react error boundaries
@@ -12,6 +13,7 @@ function App() {
   const [searchTerm, setSearchTerm] = React.useState("");
   const [isLoading, setIsLoading] = React.useState(false);
   const debouncedSearchTerm = useDebounce(searchTerm, 400);
+  const showSuggestions = packages.length > 0;
 
   React.useEffect(() => {
     if (debouncedSearchTerm.length > 1) {
@@ -20,28 +22,44 @@ function App() {
         setIsLoading(false);
         setPackages(packages);
       });
+    } else {
+      setPackages([]);
     }
   }, [debouncedSearchTerm]);
 
   return (
     <div className="App">
-      <p>Hello</p>
-      <input
-        placeholder="find package"
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {isLoading && <div>Loading ...</div>}
-      <ul>
-        {packages.map(({ package: npmPackage }) => {
-          return (
-            <li key={`${npmPackage.name}-${npmPackage.version}`}>
-              {npmPackage.name}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="container">
+        <Logo />
+        <h1 class="main-title">
+          <span class="first-word">DAVID</span>
+          <span>PHOEBIA</span>
+        </h1>
+        <p class="main-description">
+          find the cost of adding a npm package to your bundle
+        </p>
+        <div class="search-container">
+          <input
+            class={`search ${showSuggestions ? "search-with-suggestions" : ""}`}
+            placeholder="find package"
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {isLoading && <div>Loading ...</div>}
+          {!showSuggestions ? null : (
+            <ul class="suggestions">
+              {packages.map(({ package: npmPackage }) => {
+                return (
+                  <li key={`${npmPackage.name}-${npmPackage.version}`}>
+                    {npmPackage.name}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
